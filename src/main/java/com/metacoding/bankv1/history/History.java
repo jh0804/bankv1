88bc1de77c5f693e5b3066112ec24b922e187364
jay <wngml0708@gmail.com>
2025-03-24 15:44:17 +0900

    table & dummy

---
+++ src/main/java/com/metacoding/bankv1/history/History.java
@@ -0,0 +1,23 @@
+package com.metacoding.bankv1.history;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.sql.Timestamp;
+
+@NoArgsConstructor
+@Getter
+@Table(name = "history_tb")
+@Entity
+public class History { // 어느 계좌가 어느 계좌에게
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private Integer withdrawNumber; // 1111 (FK)
+    private Integer depositNumber; // 2222 (FK)
+    private Integer amount; // 100원
+    private Integer withdrawBalance; // 900원 이체되는 그 시점의 잔액 의미
+    private Timestamp createdAt;
+}
