2ab678d81234bb6b9b6d35681999e4c264f20439
jay <wngml0708@gmail.com>
2025-03-25 12:43:15 +0900

    인증 로직 완료

--- src/main/java/com/metacoding/bankv1/user/UserController.java
+++ src/main/java/com/metacoding/bankv1/user/UserController.java
@@ -1,5 +1,6 @@
 package com.metacoding.bankv1.user;
 
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +11,23 @@ import org.springframework.web.bind.annotation.PostMapping;
 public class UserController {
 
     private final UserService userService;
+    private final HttpSession session;
+
+    @GetMapping("/logout")
+    public String logout() {
+        // 세션은 보통 통으로 버린다. (removeAttribute()는 특정 key만 버림)
+        session.invalidate();
+        return "redirect:/";
+    }
+
+    // 로그인만 예외로 @Post (조회시에도)
+    @PostMapping("/login")
+    public String login(UserRequest.LoginDTO loginDTO) {
+        User sessionUser = userService.로그인(loginDTO); // service로부터 받은 user를 세션에 저장
+        session.setAttribute("sessionUser", sessionUser); // stateful
+        // getAttribute할때 sessionId로 구분된 sessionUser key의 value를 꺼낸다.
+        return "redirect:/";
+    }
 
     @GetMapping("/login-form")
     public String loginForm() {
--- src/main/java/com/metacoding/bankv1/user/UserService.java
+++ src/main/java/com/metacoding/bankv1/user/UserService.java
@@ -9,7 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 // 기능을 정리하면 여기에 전부 다 있다.
 public class UserService {
     private final UserRepository userRepository;
-    
+
     @Transactional
     public void 회원가입(UserRequest.JoinDTO joinDTO) {
         // 1. 동일 유저 네임 있는지 검사
@@ -18,5 +18,27 @@ public class UserService {
         if (user != null) throw new RuntimeException("동일한 username이 존재합니다.");
         // 3. 없으면 회원가입하기
         userRepository.save(joinDTO.getUsername(), joinDTO.getPassword(), joinDTO.getFullname());
+    }
+
+    // 클라이언트 측에는 id/pw 뭐 틀렸는지 알려줄 필요X
+    // 서버는 id/pw 둘 중 뭐가 틀렸는지 알아야 한다.
+    public User 로그인(UserRequest.LoginDTO loginDTO) {
+        // 필터링 하는 방식으로
+        // 1. 동일 유저 네임 있는지 검사
+        User user = userRepository.findByUsername(loginDTO.getUsername());
+
+        // 2. 필터링 (유저네임, 패스워드가 불일치하는 것들을)
+        // 2-1. 해당 username이 없을 경우
+        if (user == null) {
+            throw new RuntimeException("해당 username이 없습니다.");
+        }
+
+        // 2-2. 유저가 있으면 pw 비교 (pw가 일치하지 않는다면)
+        if (!(user.getPassword().equals(loginDTO.getPassword()))) {
+            throw new RuntimeException("pw가 틀렸습니다.");
+        }
+
+        // 3. 인증
+        return user;
     }
 }
